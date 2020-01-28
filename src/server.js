25b939c12fc6935779308100107dd3176f721a75
Tony Jefferson <tony@mail.rit.edu>
2020-01-28 00:26:10 -0500

    First commit

--- src/server.js
+++ src/server.js
@@ -5,17 +5,16 @@ const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
-  if (request.url == "/random"){
-  	response.writeHead(200, { 'Content-Type': 'text/plain' });
-  	response.write(`Hello - ${shortid.generate()}`);
-  	response.end();
-  } else{
-  	response.writeHead(200, { 'Content-Type': 'text/html' });
-  	response.write("<html><body><h1>Default Page!!!</h1></body></html>");
-  	response.end();
+  if (request.url === '/random') {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(`Hello - ${shortid.generate()}`);
+    response.end();
+  } else {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<html><body><h1>Default Page!!!</h1></body></html>');
+    response.end();
   }
-  
-}
+};
 
 http.createServer(onRequest).listen(port);
 
